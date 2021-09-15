43ee627b4fb0210d7bcc1ea65f7c560d2f1712ec
yurifreitas <yuribzfreitas@gmail.com>
2021-09-15 05:18:30 -0300

    tabela-tarifa(feature):First commit whith config

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from '../styles/globalStyle'
+import { theme } from '../styles/theme'
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
